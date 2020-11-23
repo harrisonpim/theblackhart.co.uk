ac3145cd38b3d40c08fb150f94df937d9a0854e8
harrisonpim <harrison.stroma@gmail.com>
2020-11-23 18:24:33 +0000

    wrap 404 message in defaultLayout

--- pages/404.js
+++ pages/404.js
@@ -1,3 +1,9 @@
+import DefaultLayout from "../layouts/default";
+
 export default function Custom404() {
-  return <h1>404 - Page Not Found</h1>;
+  return (
+    <DefaultLayout parentHref="/" parentText="Home">
+      <h1>404 - Page Not Found</h1>
+    </DefaultLayout>
+  );
 }
