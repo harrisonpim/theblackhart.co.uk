cf87f02cedb8c20dc94a87bcdea3365abdc1c42a
harrisonpim <harrison.stroma@gmail.com>
2021-02-23 10:41:23 +0000

    switch dev commands to vercel

--- pages/404.js
+++ pages/404.js
@@ -2,7 +2,7 @@ import Layout from "../components/layouts/default";
 
 export default function Custom404() {
   return (
-    <Layout title="404">
+    <Layout title="404" description="page not found">
       <div className="w-full text-center">
         <h1 className="thornletter">page not found</h1>
       </div>
