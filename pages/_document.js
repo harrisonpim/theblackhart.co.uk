27095a4ed0c21d7b1805cfce2b6f7d372e5644c6
harrisonpim <harrison.stroma@gmail.com>
2021-02-28 13:47:39 +0000

    improve favicon coverage

--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,13 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <meta charSet="utf-8" />
-          <link rel="icon" href="/favicon.ico" type="image/png" />
+          <link rel="icon" href="/icons/favicon/tbh.svg" type="image/svg+xml" />
+          <link rel="icon" href="/icons/favicon/32.ico" type="image/png" />
+          <link
+            rel="apple-touch-icon"
+            href="/icons/favicon/apple-touch-icon.png"
+          />
+          <link rel="manifest" href="/manifest.webmanifest" />
         </Head>
         <body>
           <Main />
