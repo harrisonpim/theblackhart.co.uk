a8a99cfc8c8ab86049379b509fa017c2c89ac01f
harrisonpim <harrison.stroma@gmail.com>
2021-03-27 17:41:20 +0000

    try again

--- tests/social.test.ts
+++ tests/social.test.ts
@@ -1,7 +1,7 @@
 import { socialUrl } from "./urls";
 
 describe("As a visitor from a social channel, I want to know where to find tbh on other platforms", () => {
-  test("Can see products", async () => {
+  test("Can see links", async () => {
     await page.goto(socialUrl);
     expect(await page.isVisible("ul >> li")).toBeTruthy();
   });
