a4346f4b1d5a71282397e0eb36b222d7ff5303f9
harrisonpim <hello@harrisonpim.com>
2023-03-26 20:36:01 +0100

    add structured data script to pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,6 +14,32 @@ export default function MyApp({ Component, pageProps }) {
       <Head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        {/* local schema markup */}
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: `{
+            "@context": "https://schema.org",
+            "@type": "JewelryStore",
+            "name": "The Black Hart",
+            "image": "https://images.prismic.io/theblackhart/44cd81bf-c071-4c6f-84aa-e29299741872_616f657c-82b5-4bc9-ae55-c44dc45a72ba_tbh.jpg?auto=compress,format",
+            "url": "https://theblackhart.co.uk",
+            "priceRange": "££",
+            "sameAs": [
+              "https://www.instagram.com/tbh_jewellery/",
+              "https://www.tiktok.com/@tbh_jewellery",
+              "https://www.youtube.com/channel/UCfUqXisYyNzQcRDPxP_SizQ"
+            ],
+            "image": [
+              "https://images.prismic.io/theblackhart/d503a6cb-67d2-436d-8ab0-a93fd7e23c61_IMG_7694.JPG?auto=compress,format"
+            ],
+            "areaServed": {
+              "@type": "Country",
+              "name": "United Kingdom"
+            }
+          }`,
+          }}
+        />
       </Head>
       <Component {...pageProps} />
     </CartProvider>
--- pages/shop/[uid].tsx
+++ pages/shop/[uid].tsx
@@ -1,6 +1,7 @@
 import { Client, linkResolver } from '../../prismic.config'
 import { chainLengths, ringSizes, topSizes } from '../../components/product'
 
+import Head from 'next/head'
 import ImageGallery from 'components/imageGallery'
 import Layout from '../../components/layout'
 import { RichText } from 'prismic-reactjs'
@@ -73,103 +74,128 @@ export default function ProductPage({ product, details }) {
   }
 
   return (
-    <Layout title={title} description={description}>
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
-        <div className="top-0">
-          <ImageGallery images={images} />
-        </div>
-        <div>
-          <h1 className="leading-none lg:pt-2">{title}</h1>
-          <h2 className="text-silver pb-4 text-lg font-crimson">
-            {displayPrice}
-          </h2>
-          <div className="prose">
-            <RichText
-              render={product.data.description}
-              linkResolver={linkResolver}
-            />
-            {isRing ? (
-              <div className="flex">
-                <div className="pr-2">Ring size:</div>
-                <select
-                  name="ringSizes"
-                  id="ringSizes"
-                  title="ringSizes"
-                  aria-label="ringSizes"
-                  defaultValue={ringSizes[0]}
-                  onChange={(e) => setRingSize(e.target.value)}
-                >
-                  {ringSizes.map((size) => (
-                    <option key={size} value={size}>
-                      {size}
-                    </option>
-                  ))}
-                </select>
-              </div>
-            ) : null}
-            {isNecklace ? (
-              <div className="flex">
-                <div className="pr-2">Chain length:</div>
-                <select
-                  name="chainLengths"
-                  id="chainLengths"
-                  title="chainLengths"
-                  aria-label="chainLengths"
-                  defaultValue={chainLengths[0]}
-                  onChange={(e) => setChainLength(e.target.value)}
-                >
-                  {chainLengths.map((size) => (
-                    <option key={size} value={size}>
-                      {size}
-                    </option>
-                  ))}
-                </select>
-              </div>
-            ) : null}
-            {isTop ? (
-              <div className="flex">
-                <div className="pr-2">Size:</div>
-                <select
-                  name="topSizes"
-                  id="topSizes"
-                  title="topSizes"
-                  aria-label="topSizes"
-                  defaultValue={topSizes[0]}
-                  onChange={(e) => setTopSize(e.target.value)}
-                >
-                  {topSizes.map((size) => (
-                    <option key={size} value={size}>
-                      {size}
-                    </option>
-                  ))}
-                </select>
-              </div>
-            ) : null}
-            <details>
-              <summary className="font-bold">
-                {RichText.asText(details.data.title)}
-              </summary>
-              <div className="text-sm pt-2">
-                <RichText
-                  render={details.data.text}
-                  linkResolver={linkResolver}
-                />
-              </div>
-            </details>{' '}
+    <>
+      <Head>
+        <script type="application/ld+json">
+          {JSON.stringify({
+            '@context': 'https://schema.org',
+            '@type': 'Product',
+            name: title,
+            image: images.map((image) => image.image.url),
+            description,
+            sku: product.uid,
+            brand: {
+              '@type': 'Brand',
+              name: 'The Black Hart',
+            },
+            offers: {
+              '@type': 'Offer',
+              url: `https://theblackhart.co.uk/shop/${product.uid}`,
+              priceCurrency: 'GBP',
+              price: product.data.price / 100,
+              availability: 'https://schema.org/InStock',
+            },
+          })}
+        </script>
+      </Head>
+      <Layout title={title} description={description}>
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+          <div className="top-0">
+            <ImageGallery images={images} />
           </div>
-          <div className="pt-3">
-            <button
-              onClick={() => {
-                addItem(productData, options)
-                window.location.href = '/shop/basket'
-              }}
-            >
-              Add to basket
-            </button>
+          <div>
+            <h1 className="leading-none lg:pt-2">{title}</h1>
+            <h2 className="text-silver pb-4 text-lg font-crimson">
+              {displayPrice}
+            </h2>
+            <div className="prose">
+              <RichText
+                render={product.data.description}
+                linkResolver={linkResolver}
+              />
+              {isRing ? (
+                <div className="flex">
+                  <div className="pr-2">Ring size:</div>
+                  <select
+                    name="ringSizes"
+                    id="ringSizes"
+                    title="ringSizes"
+                    aria-label="ringSizes"
+                    defaultValue={ringSizes[0]}
+                    onChange={(e) => setRingSize(e.target.value)}
+                  >
+                    {ringSizes.map((size) => (
+                      <option key={size} value={size}>
+                        {size}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+              ) : null}
+              {isNecklace ? (
+                <div className="flex">
+                  <div className="pr-2">Chain length:</div>
+                  <select
+                    name="chainLengths"
+                    id="chainLengths"
+                    title="chainLengths"
+                    aria-label="chainLengths"
+                    defaultValue={chainLengths[0]}
+                    onChange={(e) => setChainLength(e.target.value)}
+                  >
+                    {chainLengths.map((size) => (
+                      <option key={size} value={size}>
+                        {size}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+              ) : null}
+              {isTop ? (
+                <div className="flex">
+                  <div className="pr-2">Size:</div>
+                  <select
+                    name="topSizes"
+                    id="topSizes"
+                    title="topSizes"
+                    aria-label="topSizes"
+                    defaultValue={topSizes[0]}
+                    onChange={(e) => setTopSize(e.target.value)}
+                  >
+                    {topSizes.map((size) => (
+                      <option key={size} value={size}>
+                        {size}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+              ) : null}
+              <details>
+                <summary className="font-bold">
+                  {RichText.asText(details.data.title)}
+                </summary>
+                <div className="text-sm pt-2">
+                  <RichText
+                    render={details.data.text}
+                    linkResolver={linkResolver}
+                  />
+                </div>
+              </details>{' '}
+            </div>
+            <div className="pt-3">
+              <button
+                onClick={() => {
+                  addItem(productData, options)
+                  window.location.href = '/shop/basket'
+                }}
+              >
+                Add to basket
+              </button>
+            </div>
           </div>
         </div>
-      </div>
-    </Layout>
+      </Layout>
+    </>
   )
 }
 
